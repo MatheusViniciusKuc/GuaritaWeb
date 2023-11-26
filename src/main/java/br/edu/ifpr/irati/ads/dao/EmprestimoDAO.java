6dd814bf0053e84d0a6ace296bf3f00533951b6c
Matheus Vinicius Kuc <matheus.v.kuc@gmail.com>
2023-11-26 13:50:25 -0300

    feat: Validação de Conversão Data
    No relatório de Empréstimos.

--- src/main/java/br/edu/ifpr/irati/ads/dao/EmprestimoDAO.java
+++ src/main/java/br/edu/ifpr/irati/ads/dao/EmprestimoDAO.java
@@ -4,7 +4,11 @@ import br.edu.ifpr.irati.ads.exception.PersistenceException;
 import br.edu.ifpr.irati.ads.model.Emprestimo;
 import br.edu.ifpr.irati.ads.model.Espaco;
 import br.edu.ifpr.irati.ads.model.Status;
+import br.edu.ifpr.irati.ads.util.Util;
+import jakarta.persistence.Query;
+import java.text.ParseException;
 import java.util.Date;
+import java.util.List;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
@@ -33,6 +37,36 @@ public class EmprestimoDAO extends GenericDAO<Emprestimo> {
             return count == 0;
         } catch (HibernateException e) {
             throw new PersistenceException(e.getMessage());
+        }
+    }
+
+    public List<Emprestimo> filtrarRelatorio(Date dateInicioFiltro, Date dateFinalFiltro)
+            throws PersistenceException, ParseException {
+        try {
+            String hql = "FROM Emprestimo e "
+                    + "WHERE 1 = 1";
+
+            if (dateInicioFiltro != null) {
+                hql += " AND e.dataInicio >= :dataInicio";
+            }
+            if (dateFinalFiltro != null) {
+                hql += " AND e.dataFim <= :dataFim";
+            }
+
+            Query query = session.createQuery(hql);
+
+            if (dateInicioFiltro != null) {
+                dateInicioFiltro = Util.configurarDate(dateInicioFiltro, "01:00");
+                query.setParameter("dataInicio", dateInicioFiltro);
+            }
+            if (dateFinalFiltro != null) {
+                dateFinalFiltro = Util.configurarDate(dateFinalFiltro, "23:00");
+                query.setParameter("dataFim", dateFinalFiltro);
+            }
+
+            return query.getResultList();
+        } catch (HibernateException he) {
+            throw new PersistenceException(he.getMessage());
         }
     }
 }
